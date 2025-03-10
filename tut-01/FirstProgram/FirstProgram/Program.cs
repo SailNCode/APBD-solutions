7e43add786245e66bdc70feeb90b04dd8b34e852
sailncode <kkamilstefanski@gmail.com>
2025-03-10 21:30:48 +0100

    feat(tut-01): add average function

--- tut-01/FirstProgram/FirstProgram/Program.cs
+++ tut-01/FirstProgram/FirstProgram/Program.cs
@@ -6,5 +6,16 @@ internal class Program
     {
         Console.WriteLine("Hello, World!");
         Console.WriteLine("My name is Kamil");
+        Console.WriteLine(CalcAverage(new int[]{1, 4, 5, 8}));
+    }
+
+    public static double CalcAverage(int[] numbers)
+    {
+        double sum = 0.0;
+        foreach (var number in numbers)
+        {
+            sum += number;
+        }
+        return sum/numbers.Length;
     }
 }
